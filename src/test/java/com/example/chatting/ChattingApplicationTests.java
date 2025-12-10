bf698f1095d00c468964c045e8c1406b2634fe47
장현우 <90463719+fsdawer@users.noreply.github.com>
2025-12-10 10:12:39 +0900

    Update ChattingApplicationTests.java

--- src/test/java/com/example/chatting/ChattingApplicationTests.java
+++ src/test/java/com/example/chatting/ChattingApplicationTests.java
@@ -3,11 +3,6 @@ package com.example.chatting;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ChattingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
